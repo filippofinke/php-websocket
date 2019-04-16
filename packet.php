92064acd320c80374281bbe6db603bb2e8386191
Filippo Finke <filippo.finke@samtrevano.ch>
2019-04-16 18:24:09 +0200

    Code style fix

--- packet.php
+++ packet.php
@@ -1,130 +1,135 @@
 <?php
 
-class Packet {
+class Packet
+{
+    private $bytes;
 
-	private $bytes;
+    public function __construct($string)
+    {
+        $this->bytes = array_values(unpack('C*', $string));
+    }
 
-	public function __construct($string)
-	{
-		$this->bytes = array_values(unpack('C*', $string));
-  	}
+    public function log()
+    {
+        file_put_contents(
+          "info.log",
+          "Fin ".$this->getFin()."\n".
+        "Rsv1 ".$this->getRsv1()."\n".
+        "Rsv2 ".$this->getRsv2()."\n".
+        "Rsv3 ".$this->getRsv3()."\n".
+        "Opcode ".$this->getOpcode()."\n".
+        "isMasked ".$this->isMasked()."\n".
+                "PayloadTempLength ".$this->getPayloadTempLength()."\n".
+        "PayloadLength ".$this->getPayloadLength()."\n".
+        "MaskOffset ".$this->getMaskOffset()."\n".
+        "PayloadOffset ".$this->getPayloadOffset()."\n"
+      );
+        file_put_contents("bytes.log", json_encode($this->getBytes(), true));
+        file_put_contents("mask.log", json_encode($this->getMask(), true));
+        file_put_contents("payload.log", json_encode($this->getPayload(), true));
+    }
 
-  	public function log() {
-  	  file_put_contents(
-  	  	"info.log",
-  	  	"Fin ".$this->getFin()."\n".
-  	  	"Rsv1 ".$this->getRsv1()."\n".
-  	  	"Rsv2 ".$this->getRsv2()."\n".
-  	  	"Rsv3 ".$this->getRsv3()."\n".
-  	  	"Opcode ".$this->getOpcode()."\n".
-  	  	"isMasked ".$this->isMasked()."\n".
-				"PayloadTempLength ".$this->getPayloadTempLength()."\n".
-  	  	"PayloadLength ".$this->getPayloadLength()."\n".
-  	  	"MaskOffset ".$this->getMaskOffset()."\n".
-  	  	"PayloadOffset ".$this->getPayloadOffset()."\n"
-  	  );
-  	  file_put_contents("bytes.log", json_encode($this->getBytes(), true));
-      file_put_contents("mask.log", json_encode($this->getMask(), true));
-      file_put_contents("payload.log", json_encode($this->getPayload(), true));
-  	}
+    public function getBytes()
+    {
+        return $this->bytes;
+    }
 
-  	public function getBytes() {
-  		return $this->bytes;
-  	}
+    public function getBit($index, $number)
+    {
+        return ($number >> $index) & 1;
+    }
 
-  	public function getBit($index, $number)
-	{
-	  	return ($number >> $index) & 1;
-	}
+    public function getFin()
+    {
+        return $this->getBit(7, $this->bytes[0]);
+    }
 
-	public function getFin() {
-	  	return $this->getBit(7, $this->bytes[0]);
-	}
+    public function getRsv1()
+    {
+        return $this->getBit(6, $this->bytes[0]);
+    }
 
-	public function getRsv1() {
-		return $this->getBit(6, $this->bytes[0]);
-	}
+    public function getRsv2()
+    {
+        return $this->getBit(5, $this->bytes[0]);
+    }
 
-	public function getRsv2() {
-		return $this->getBit(5, $this->bytes[0]);
-	}
+    public function getRsv3()
+    {
+        return $this->getBit(4, $this->bytes[0]);
+    }
 
-	public function getRsv3() {
-		return $this->getBit(4, $this->bytes[0]);
-	}
+    public function getOpcode()
+    {
+        return $this->bytes[0] & 0b00001111;
+    }
 
-	public function getOpcode() {
-		return $this->bytes[0] & 0b00001111;
-	}
+    public function isMasked()
+    {
+        return $this->getBit(7, $this->bytes[1]);
+    }
 
-	public function isMasked() {
-		return $this->getBit(7, $this->bytes[1]);
-	}
+    public function getPayloadTempLength()
+    {
+        $length = $this->bytes[1] & 0b01111111;
+        return $length;
+    }
 
-	public function getPayloadTempLength() {
-		$length = $this->bytes[1] & 0b01111111;
-		return $length;
-	}
+    public function getPayloadLength()
+    {
+        $length = $this->getPayloadTempLength();
+        if ($length == 126) {
+            $int16 = pack("C*", $this->bytes[2], $this->bytes[3]);
+            $length = unpack("n", $int16)[1];
+        } elseif ($length == 127) {
+            $int64 = pack("C*", $this->bytes[2], $this->bytes[3], $this->bytes[4], $this->bytes[5], $this->bytes[6], $this->bytes[7], $this->bytes[8], $this->bytes[9]);
+            $length = unpack("Q", $int64)[1];
+        }
+        return $length;
+    }
 
-	public function getPayloadLength() {
-		$length = $this->getPayloadTempLength();
-		if($length == 126)
-		{
-			$int16 = pack("C*", $this->bytes[2],$this->bytes[3]);
-			$length = unpack("n", $int16)[1];
-		}
-		else if($length == 127)
-		{
-			$int64 = pack("C*", $this->bytes[2],$this->bytes[3],$this->bytes[4],$this->bytes[5],$this->bytes[6],$this->bytes[7],$this->bytes[8],$this->bytes[9]);
-			$length = unpack("Q", $int64)[1];
-		}
-		return $length;
-	}
+    public function getMaskOffset()
+    {
+        $length = $this->getPayloadTempLength();
+        $offset = 2;
+        if ($length == 126) {
+            $offset += 2;
+        } elseif ($length == 127) {
+            $offset += 8;
+        }
+        return $offset;
+    }
 
-	public function getMaskOffset() {
-		$length = $this->getPayloadTempLength();
-		$offset = 2;
-		if($length == 126)
-		{
-			$offset += 2;
-		}
-		else if($length == 127)
-		{
-			$offset += 8;
-		}
-		return $offset;
-	}
+    public function getPayloadOffset()
+    {
+        return $this->getMaskOffset() + 4;
+    }
 
-	public function getPayloadOffset() {
-		return $this->getMaskOffset() + 4;
-	}
+    public function getMask()
+    {
+        $mask = array();
+        $offset = $this->getMaskOffset();
+        $mask[] = $this->bytes[$offset];
+        $mask[] = $this->bytes[$offset + 1];
+        $mask[] = $this->bytes[$offset + 2];
+        $mask[] = $this->bytes[$offset + 3];
+        return $mask;
+    }
 
-	public function getMask() {
-		$mask = array();
-		$offset = $this->getMaskOffset();
-		$mask[] = $this->bytes[$offset];
-		$mask[] = $this->bytes[$offset + 1];
-		$mask[] = $this->bytes[$offset + 2];
-		$mask[] = $this->bytes[$offset + 3];
-		return $mask;
-	}
+    public function getPayload()
+    {
+        $payload = array_slice($this->bytes, $this->getPayloadOffset());
+        return $payload;
+    }
 
-	public function getPayload() {
-		$payload = array_slice($this->bytes, $this->getPayloadOffset());
-		return $payload;
-	}
-
-	public function getPayloadString() {
-		$string = "";
-		$key = $this->getMask();
-		$payload = $this->getPayload();
-		for($i = 0; $i < count($payload); $i++)
-		{
-			$string .= chr($payload[$i] ^ $key[$i % 4]);
-		}
-		return $string;
-	}
-
+    public function getPayloadString()
+    {
+        $string = "";
+        $key = $this->getMask();
+        $payload = $this->getPayload();
+        for ($i = 0; $i < count($payload); $i++) {
+            $string .= chr($payload[$i] ^ $key[$i % 4]);
+        }
+        return $string;
+    }
 }
-
-?>
--- server.php
+++ server.php
@@ -5,137 +5,125 @@
 */
 require 'packet.php';
 
-class Server {
+class Server
+{
 
   /**
   * See https://tools.ietf.org/html/rfc6455
   */
-  private $token = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+    private $token = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
 
-  private $debug = false;
+    private $debug = false;
 
-  private $port;
+    private $port;
 
-  private function getSignature($key) {
-    return base64_encode(sha1($key.$this->token, true));
-  }
-
-  private function lastError() {
-    $errorcode = socket_last_error();
-    $errormsg = socket_strerror($errorcode);
-    $this->log("[$errorcode] $errormsg", "ERROR");
-  }
-
-  public function debug() {
-    $this->debug = !$this->debug;
-  }
-
-  public function log($text, $type = 'info') {
-    if($this->debug)
+    private function getSignature($key)
     {
-      echo date("H:i:s d/m/Y")." ".$type." - ".$text.PHP_EOL;
+        return base64_encode(sha1($key.$this->token, true));
     }
-  }
 
-  public function send($message, $socket)
-  {
-
-  }
-
-  public function read($socket) {
-    $data = "";
-    $bytes = 1;
-    while($bytes > 0)
+    private function lastError()
     {
-      $bytes = socket_recv($socket, $data, 2048, 0);
-      $packet = new Packet($data);
-      $packet->log();
-      $this->log($packet->getPayLoadLength()."# ".$packet->getPayloadString());
+        $errorcode = socket_last_error();
+        $errormsg = socket_strerror($errorcode);
+        $this->log("[$errorcode] $errormsg", "ERROR");
     }
-    $this->lastError();
-    $this->log("Client disconnected");
-  }
 
-  public function start() {
-    $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-    if($socket === false)
+    public function debug()
     {
-      $this->log($this->lastError());
+        $this->debug = !$this->debug;
     }
-    else
+
+    public function log($text, $type = 'info')
     {
-      $this->log("Socket created");
-      if(socket_bind($socket, "127.0.0.1", $this->port))
-      {
-        $this->log("Socket is now binded: 127.0.0.1 ".$this->port);
-        if(socket_listen($socket))
-        {
-          $this->log("Socket is listening");
-          socket_set_nonblock($socket);
-          while(true)
-          {
-            $client = socket_accept($socket);
-            if($client !== false)
-            {
-              $startTime = microtime();
-              $this->log("New client connected - Handshake");
-              $handshake = "";
-              $read = "";
-              $received = false;
-              while(!($read == "" && $received))
-              {
-                  $read = socket_read($client, 1024);
-                  if($read != "")
-                  {
-                    $handshake = $handshake."".$read;
-                    $received = true;
-                  }
-              }
-              preg_match_all('/Sec-WebSocket-Key: (.*?)\n/s', $handshake, $matches);
-              $key = trim($matches[1][0]);
-              $signature = $this->getSignature($key);
-              $this->log($key." - ".$signature);
-              $response = "HTTP/1.1 101 Switching Protocols\r\n".
+        if ($this->debug) {
+            echo date("H:i:s d/m/Y")." ".$type." - ".$text.PHP_EOL;
+        }
+    }
+
+    public function send($message, $socket)
+    {
+    }
+
+    public function read($socket)
+    {
+        $data = "";
+        $bytes = 1;
+        while ($bytes > 0) {
+            $bytes = socket_recv($socket, $data, 2048, 0);
+            $packet = new Packet($data);
+            $packet->log();
+            $this->log($packet->getPayLoadLength()."# ".$packet->getPayloadString());
+        }
+        $this->lastError();
+        $this->log("Client disconnected");
+    }
+
+    public function start()
+    {
+        $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+        if ($socket === false) {
+            $this->log($this->lastError());
+        } else {
+            $this->log("Socket created");
+            if (socket_bind($socket, "127.0.0.1", $this->port)) {
+                $this->log("Socket is now binded: 127.0.0.1 ".$this->port);
+                if (socket_listen($socket)) {
+                    $this->log("Socket is listening");
+                    socket_set_nonblock($socket);
+                    while (true) {
+                        $client = socket_accept($socket);
+                        if ($client !== false) {
+                            $startTime = microtime();
+                            $this->log("New client connected - Handshake");
+                            $handshake = "";
+                            $read = "";
+                            $received = false;
+                            while (!($read == "" && $received)) {
+                                $read = socket_read($client, 1024);
+                                if ($read != "") {
+                                    $handshake = $handshake."".$read;
+                                    $received = true;
+                                }
+                            }
+                            preg_match_all('/Sec-WebSocket-Key: (.*?)\n/s', $handshake, $matches);
+                            $key = trim($matches[1][0]);
+                            $signature = $this->getSignature($key);
+                            $this->log($key." - ".$signature);
+                            $response = "HTTP/1.1 101 Switching Protocols\r\n".
                           "Upgrade: websocket\r\n".
                           "Connection: Upgrade\r\n".
                           "Sec-WebSocket-Accept: $signature\r\n\r\n";
-              if(socket_write($client, $response) === false)
-              {
-                $this->lastError();
-              }
-              else {
-                $this->log("Handshake end");
+                            if (socket_write($client, $response) === false) {
+                                $this->lastError();
+                            } else {
+                                $this->log("Handshake end");
 
-                $pid = pcntl_fork();
-                if(!$pid) {
-                  socket_set_block($client);
-                  $this->log($key." Process forked");
-                  $this->read($client);
-                  socket_close($client);
+                                $pid = pcntl_fork();
+                                if (!$pid) {
+                                    socket_set_block($client);
+                                    $this->log($key." Process forked");
+                                    $this->read($client);
+                                    socket_close($client);
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    $this->log($this->lastError());
                 }
-
-              }
+            } else {
+                $this->log($this->lastError());
             }
-          }
         }
-        else {
-          $this->log($this->lastError());
-        }
-      }
-      else {
-        $this->log($this->lastError());
-      }
     }
-  }
 
-  public function __construct($port)
-  {
-    $this->port = $port;
-  }
-
+    public function __construct($port)
+    {
+        $this->port = $port;
+    }
 }
 
 $s = new Server($argv[1]);
 $s->debug();
 $s->start();
-?>
